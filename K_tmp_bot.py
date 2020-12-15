74450974b59f69c9861e0cdfbf1d16cc394947d3
comradecoastal <endermedia12@gmail.com>
2020-12-16 01:37:42 +0500

    Создан пример бота, получающего на ввод xlsx файл, данные которого он преобразует в строки. В этом случае бот отсылает данные, содержащиеся в файле позльвателю.

---
+++ K_tmp_bot.py
@@ -0,0 +1,34 @@
+#  A temporary bot for testing purposes
+
+from _token import token
+import telebot
+from K_ecxel_array import *
+
+bot = telebot.TeleBot(token)
+url_expected = False
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id,
+                     "I am a robot. I have no heart. My only job is to take your excel files "
+                     "and to convert them to arrays.")
+
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    pass
+
+
+@bot.message_handler(content_types=['document'])
+def file_handler(message):
+    data = bot.get_file(message.document.file_id)
+    print(data)
+    url = 'https://api.telegram.org/file/bot{}/{}'.format(token, data.file_path)
+    x, y = excel_array_url(url)
+
+    bot.send_message(message.chat.id,
+                     'Your values are: \nx: ' + str(x) + '\ny: ' + str(y))
+
+
+bot.polling(none_stop=True)

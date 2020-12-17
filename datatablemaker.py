7b39f42a544436e56167ce73ba0dbbc6a5c6bbf5
andreyshmipt <shulga.ad@phystech.edu>
2020-12-18 01:56:43 +0300

    Тут можно последовательно заполнить таблицу, пока возможные ошибки не обработаны, плюс надо добавить модуль пересчета погрешностей. Добавлена несерьезная обработка серьезных ошибок, в идеале надо сделать так чтобы при ошибке несоответствия длины массива и таблицы сразу можно было бы ввести исправленный массив,а не заново писать, что-нибудь , дальше плот и так далее...

--- datatablemaker.py
+++ datatablemaker.py
@@ -4,10 +4,21 @@ from telebot import types
 
 bot = telebot.TeleBot("1489374378:AAEKhKVW91XaEgRybkOwvXRM2vjmBLInzQc", parse_mode=None)
 
-data = pd.DataFrame()
+error = 0
+
+@bot.message_handler(commands=['/restart'])
+def restart(message):
+    global error
+    bot.send_message('Что-то сломалось) , бот перезапущен, напишите что-нибудь')
+    error = 0
+    bot.register_next_step_handler(message, start)
+
 
 @bot.message_handler(content_types=['text'])
 def start(message):
+    global data
+    if error != 1:
+        data=pd.DataFrame()
     if message.text == '/plot':
         keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
         keyboard1.row('Да', 'Нет')
@@ -33,14 +44,21 @@ def column_naming(message):
     bot.send_message(message.from_user.id, 'секция с названием')
     global column_title
     column_title = str(message.text)
-    bot.send_message(message.from_user.id, 'Введите данные соответствующие переменной(столбца)')
+    bot.send_message(message.from_user.id, 'Введите данные соответствующие переменной(столбцу)')
     bot.register_next_step_handler(message, table_content_change)
 
 
 @bot.message_handler(content_types=['text'])
 def table_content_change(message):
     global column_title
+    global error
     bot.send_message(message.from_user.id, 'aaaaaa')
+    try:
+        data[column_title] = message.text.split()
+    except BaseException:
+        bot.send_message(message.from_user.id, 'Ошибка, следите за количеством элементов. Введите переменные данные соответствующие этой переменной(столбцу) еще раз')
+        error = 1
+        return
     data[column_title] = message.text.split()
     result = ' '.join(data.columns) + '\n'
     a = data.values
@@ -59,6 +77,10 @@ def data_show(message):
     global data
     result = ' '.join(data.columns) + '\n'
     a = data.values
+    try :
+        len(data[column_title])
+    except BaseException:
+        return
     for i in range(len(data[column_title])):
         temp_string = ' '.join((a[i]))
         result += temp_string

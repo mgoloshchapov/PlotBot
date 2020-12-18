a3097ce5c4b08270bef358979342c687db68a6e9
mgoloshchapov <m.goloshchapov@yandex.ru>
2020-12-18 22:39:11 +0300

    Добавил выбор типов графиков(мнк, соединить точки)

--- M_plot.py
+++ M_plot.py
@@ -3,11 +3,14 @@ import numpy as np
 
 
 def plot(x, y,
+         grid,
          x_label,
          x_tick,
          y_label,
          y_tick,
          title,
+         cdots,  # plot connected dots
+         mnk,  # plot regression
          dot_color='black',
          line_color='black',
          line_label='',
@@ -20,18 +23,20 @@ def plot(x, y,
     plt.ylabel(y_label)
     plt.title(title)
 
-    plt.xticks([min(x) + i*x_tick for i in range(round((max(x)-min(x))/x_tick)+1)])
-    plt.yticks([min(y) + i * y_tick for i in range(round((max(y) - min(y)) / y_tick) + 1)])
-    plt.grid()
-
-    plt.plot(x, y, color=line_color, label=line_label, linestyle=linestyle)  # line
+    if grid == 'yes':
+        plt.xticks([min(x) + i*x_tick for i in range(round((max(x)-min(x))/x_tick)+1)])
+        plt.yticks([min(y) + i * y_tick for i in range(round((max(y) - min(y)) / y_tick) + 1)])
+        plt.grid()
 
     plt.scatter(x, y, color=dot_color, label=dot_label)  # dots
 
-    A = np.vstack([x, np.ones(len(x))]).T  # least square
-    m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+    if cdots == 'yes':
+        plt.plot(x, y, color=line_color, label=line_label, linestyle=linestyle)  # line
 
-    plt.plot(x, m*x + c)
+    if mnk == 'yes':
+        A = np.vstack([x, np.ones(len(x))]).T  # least square
+        m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+        plt.plot(x, m*x + c)
 
     if line_label != '' or dot_label != '':
         plt.legend()
